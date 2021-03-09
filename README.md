039ed8f535dc8aec32c2d3edc8269fd44d34582f
Celeo <mattboulanger@fastmail.com>
2021-03-08 16:55:19 -0800

    init

---
+++ README.md
@@ -0,0 +1,30 @@
+# valheim-slash-opens-chat
+
+A plugin for [Valheim](https://www.valheimgame.com/) that allows faster command execution by allowing you to open the chat window with the slash key `/` in addition to the default enter key. Doing so puts that slash key in your input as well.
+
+Basically, it saves you a single key, but it's more for the muscle memory I have for using the slash key to open chat windows in MMOs.
+
+## Building
+
+You'll need Visual Studio.
+
+1. Clone repo
+1. Create a Libs\\ directory in the project root
+1. Download BepInEx to your machine
+1. Copy in the following files to the Libs\\ directory from the game or from BepInEx:
+	- 0Harmony.dll
+	- assembly_valheim.dll
+	- BepInEx.dll
+	- UnityEngine.CoreModule.dll
+	- UnityEngine.dll
+	- UnityEngine.UI.dll
+	- UnityEngine.InputLegacyModule.dll
+
+## Using
+
+Download from [thunderstore](https://valheim.thunderstore.io) and install manually or use a mod manager.
+
+## Licensing
+
+- Code: MIT
+- icon.png originally by [Kiranshastry](https://www.flaticon.com/authors/kiranshastry) from <https://www.flaticon.com>
---
+++ SlashOpensChatPlugin/SlashOpensChatPlugin.cs
@@ -0,0 +1,76 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace SlashOpensChat
+{
+    [BepInPlugin("com.celeo.valheim.slashopenschat", "Slash Opens Chat", "1.0.0.0")]
+    public class SlashOpensChatPlugin : BaseUnityPlugin
+    {
+        private static Harmony harmonyInstance;
+        private static bool chatJustOpened = false;
+
+        void Awake()
+        {
+            Debug.Log("SlashOpensChat plugin initialized");
+            harmonyInstance = Harmony.CreateAndPatchAll(
+                    Assembly.GetExecutingAssembly(),
+                    "com.celeo.valheim.slashopenschat"
+            );
+        }
+
+        void OnDestroy()
+        {
+            harmonyInstance.UnpatchSelf();
+        }
+
+        [HarmonyPatch(typeof(Player), "Update")]
+        public static class PlayerPatch
+        {
+            private static bool ChatAlreadyOpen()
+            {
+                return Console.IsVisible()
+                    || TextInput.IsVisible()
+                    || Minimap.InTextInput()
+                    || Menu.IsVisible()
+                    || Chat.instance.m_input.gameObject.activeSelf;
+            }
+
+            private static void Postfix(Player __instance)
+            {
+                if (Player.m_localPlayer != __instance)
+                {
+                    return;
+                }
+                if (Input.GetKeyDown(KeyCode.Slash) && !ChatAlreadyOpen())
+                {
+                    // Basically do what Chat::Update() does when the enter key is pressed
+                    Chat.instance.GetType().GetField("m_hideTimer", BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance).SetValue(Chat.instance, 0f);
+                    Chat.instance.m_chatWindow.gameObject.SetActive(value: true);
+                    Chat.instance.m_input.gameObject.SetActive(value: true);
+                    Chat.instance.m_input.ActivateInputField();
+
+                    // Put the slash into the input
+                    Chat.instance.m_input.text = "/";
+
+                    // Set a flag so the chat text position can be updated
+                    chatJustOpened = true;
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(Chat), "LateUpdate")]
+        public static class ChatPatch
+        {
+            private static void Postfix(Chat __instance)
+            {
+                if (chatJustOpened)
+                {
+                    Chat.instance.m_input.MoveTextEnd(true);
+                    chatJustOpened = false;
+                }
+            }
+        }
+    }
+}
